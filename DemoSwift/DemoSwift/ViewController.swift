2da221c1b40f62a2e365fd4337586907ae77a319
wanganya <wanganya@aikucun.com>
2022-07-29 14:14:12 +0800

    工程文件提交

---
+++ DemoSwift/DemoSwift/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DemoSwift
+//
+//  Created by 安亚 on 2022/7/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func abc() {
+        print("这是个练习哦。。。。。。。")
+    }
+
+}
+
